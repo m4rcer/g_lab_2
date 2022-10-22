0c81bc4de897754326b6183ab30c514e77f91346
m4rcer <m4rcer@gmail.com>
2022-10-22 23:47:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace g_lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphPictureBox = new System.Windows.Forms.PictureBox();
+            this.pixelsButton = new System.Windows.Forms.Button();
+            this.millemetersButton = new System.Windows.Forms.Button();
+            this.inchesButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.graphPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphPictureBox
+            // 
+            this.graphPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.graphPictureBox.Name = "graphPictureBox";
+            this.graphPictureBox.Size = new System.Drawing.Size(1000, 500);
+            this.graphPictureBox.TabIndex = 0;
+            this.graphPictureBox.TabStop = false;
+            // 
+            // pixelsButton
+            // 
+            this.pixelsButton.Location = new System.Drawing.Point(116, 534);
+            this.pixelsButton.Name = "pixelsButton";
+            this.pixelsButton.Size = new System.Drawing.Size(121, 38);
+            this.pixelsButton.TabIndex = 1;
+            this.pixelsButton.Text = "Pixels";
+            this.pixelsButton.UseVisualStyleBackColor = true;
+            this.pixelsButton.Click += new System.EventHandler(this.pixelsButton_Click);
+            // 
+            // millemetersButton
+            // 
+            this.millemetersButton.Location = new System.Drawing.Point(315, 534);
+            this.millemetersButton.Name = "millemetersButton";
+            this.millemetersButton.Size = new System.Drawing.Size(121, 38);
+            this.millemetersButton.TabIndex = 2;
+            this.millemetersButton.Text = "Millemeters";
+            this.millemetersButton.UseMnemonic = false;
+            this.millemetersButton.UseVisualStyleBackColor = true;
+            this.millemetersButton.Click += new System.EventHandler(this.millemetersButton_Click);
+            // 
+            // inchesButton
+            // 
+            this.inchesButton.Location = new System.Drawing.Point(513, 534);
+            this.inchesButton.Name = "inchesButton";
+            this.inchesButton.Size = new System.Drawing.Size(121, 38);
+            this.inchesButton.TabIndex = 3;
+            this.inchesButton.Text = "Inches";
+            this.inchesButton.UseVisualStyleBackColor = true;
+            this.inchesButton.Click += new System.EventHandler(this.inchesButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(721, 534);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(121, 38);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 594);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.inchesButton);
+            this.Controls.Add(this.millemetersButton);
+            this.Controls.Add(this.pixelsButton);
+            this.Controls.Add(this.graphPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.graphPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox graphPictureBox;
+        private System.Windows.Forms.Button pixelsButton;
+        private System.Windows.Forms.Button millemetersButton;
+        private System.Windows.Forms.Button inchesButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace g_lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            graphPictureBox.BackColor = Color.FromKnownColor(KnownColor.Info);
+        }
+
+        private void DrawGrid(Graphics g, Pen pen)
+        {
+            DrawRectangle(g, pen);
+            DrawAxes(g, pen);
+        }
+
+        private void DrawRectangle(Graphics g, Pen pen)
+        {
+            g.DrawRectangle(pen, 0, 0, graphPictureBox.Size.Width -1, graphPictureBox.Size.Height - 1);
+        }
+
+        private void DrawAxes(Graphics g, Pen pen)
+        {
+            g.DrawLine(pen,
+                graphPictureBox.Size.Width / 2, 0,
+                graphPictureBox.Size.Width / 2, graphPictureBox.Size.Height);
+            g.DrawLine(pen,
+                0, graphPictureBox.Size.Height / 2,
+                graphPictureBox.Size.Width, graphPictureBox.Size.Height / 2);
+        }
+
+        private void DrawChart(Graphics g, Pen pen, double minX, double maxX, Func<double, double> function)
+        {
+
+            List<PointF> points = new List<PointF>();
+
+            for (double step = minX, i = 0; step < maxX; step += 0.01, i++)
+            {
+                double x = (step * 50) + (int)(graphPictureBox.Size.Width / 2);
+                double tmp = function(step);
+                tmp *= 100;
+                double y = (int)(graphPictureBox.Size.Height / 2) - tmp;
+                points.Add(new PointF((float)x, (float)y));
+            }
+
+            g.DrawLines(pen, points.ToArray());
+        }
+
+        private void Draw(GraphicsUnit pageUnit)
+        {
+            Pen redPen = new Pen(Color.Red, 1);
+            Pen greenPen = new Pen(Color.Blue, 1);
+            Graphics g = graphPictureBox.CreateGraphics();
+            g.PageUnit = pageUnit;
+            Func<double, double> function = x => Math.Sin(x);
+            DrawGrid(g, redPen);
+            DrawChart(g, greenPen, -2.5 * Math.PI, 2.5 * Math.PI, function);
+
+            g.Dispose();
+        }
+
+        private void pixelsButton_Click(object sender, EventArgs e)
+        {
+            Draw(GraphicsUnit.Pixel);
+        }
+
+        private void millemetersButton_Click(object sender, EventArgs e)
+        {
+            Draw(GraphicsUnit.Millimeter);
+        }
+
+        private void inchesButton_Click(object sender, EventArgs e)
+        {
+            Draw(GraphicsUnit.Inch);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = graphPictureBox.CreateGraphics();
+            g.Clear(Color.FromKnownColor(KnownColor.Info));
+            g.Dispose();
+        }
+    }
+}
